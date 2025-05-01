a204d7a028d3374b6683b6b5ab18009e6556c5fb
PwAAstud <01197780@pw.edu.pl>
2025-05-01 20:33:24 +0200

    poczateke

---
+++ cutGraf/Graf.java
@@ -0,0 +1,9 @@
+package cutGraf;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class Graf {
+    private int maxInRow;
+    private ArrayList<int> a;
+}
---
+++ cutGraf/MainTest.java
@@ -0,0 +1,7 @@
+package cutGraf;
+
+public class MainTest {
+    public static void main(String[] args) {
+        System.out.println("aaa");
+    }   
+}
